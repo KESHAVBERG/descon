25b29ac0f9038502cd507fe9265332ef5212f1c3
KESHAVBERG <kingkeshav7@gmail.com>
2022-05-21 18:09:40 +0530

    made margins

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,7 +1,10 @@
 function openMenu(){
     document.getElementById(`sideMenu`).style.marginRight = 0;
+    document.body.style.backgroundColor = `rgba(24, 14, 13, 0.18)`;
+
 }
 
 function closeMenu(){
     document.getElementById(`sideMenu`).style.marginRight = -250;
+    document.body.style.backgroundColor = `transparent`;
 }
