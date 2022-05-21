aa73fc6f93bd9716146a39a602ce11c56c8f36af
KESHAVBERG <kingkeshav7@gmail.com>
2022-05-21 18:29:57 +0530

    added swipes

--- js/sidebar.js
+++ js/sidebar.js
@@ -8,3 +8,23 @@ function closeMenu(){
     document.getElementById(`sideMenu`).style.marginRight = -250;
     document.body.style.backgroundColor = `transparent`;
 }
+var startingX, startingY, movingX , movingY ;
+function touchStart(evt){
+    startingX = evt.touches[0].clientX ;
+    startingY = evt.touches[0].clientY ;
+}
+function touchmove(evt){
+    movingX = evt.touches[0].clientX ;
+    movingY = evt.touches[0].clientY ;
+}
+function touchend(evt){
+    if(startingX+100 < movingX){
+        console.log('right');
+        closeMenu();
+
+} else if(startingX-100 > movingX){
+        console.log('left');
+        openMenu();
+
+ }
+}
