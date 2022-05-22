f292055b93c66bd94c7ac01b75a1c9ef7a1c849e
Sayad Pervez <pervez2504@gmail.com>
2022-05-22 18:50:44 +0530

    bg commit

---
+++ js/sidebar.js
@@ -0,0 +1,30 @@
+function openMenu(){
+    document.getElementById(`sideMenu`).style.marginRight = 0;
+    document.body.style.backgroundColor = `rgba(24, 14, 13, 0.18)`;
+
+}
+
+function closeMenu(){
+    document.getElementById(`sideMenu`).style.marginRight = -250;
+    document.body.style.backgroundColor = `transparent`;
+}
+var startingX, startingY, movingX , movingY ;
+function touchStart(evt){
+    startingX = evt.touches[0].clientX ;
+    startingY = evt.touches[0].clientY ;
+}
+function touchmove(evt){
+    movingX = evt.touches[0].clientX ;
+    movingY = evt.touches[0].clientY ;
+}
+function touchend(evt){
+    if(startingX+100 < movingX){
+        console.log('right');
+        closeMenu();
+
+} else if(startingX-100 > movingX){
+        console.log('left');
+        openMenu();
+
+ }
+}
